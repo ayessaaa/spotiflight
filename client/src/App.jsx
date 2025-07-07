1a196d6a536a579069581e53d300302605efbcb8
Ayessa <ayessamaedeguzman78@gmail.com>
2025-07-07 22:54:50 +0800

    it works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,39 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Logo from "./components/Logo";
 import BoardingPass from "./components/BoardingPass";
 import axios from "axios";
 
-const API_URL = "https://spotiflight.yessa.hackclub.app";
+const API_URL = "http://127.0.0.1:3000";
 
 function App() {
   const [tracks, setTracks] = useState([]);
   const [isLogin, setIsLogin] = useState(false);
 
-  useEffect(() => {
     async function getSpotifyTop() {
       try {
-        const res = await axios.get(API_URL + `/spotify-search`, {});
+        const res = await axios.get(API_URL + `/spotify-search`, {withCredentials: true,});
         console.log(res.data);
         setTracks(res.data.items);
       } catch (err) {
         console.error(err.response?.data || err.message);
       }
     }
-    getSpotifyTop();
-  }, []);
-
-  // window.location.href = "https://spotiflight.yessa.hackclub.app/login";
 
   async function getLogin() {
-    try {
-      const res = await axios.get(API_URL + `/login`, {});
-      console.log(res.data);
-    } catch (err) {
-      console.error(err.response?.data || err.message);
-    }
+    // try {
+    //   const res = await axios.get(API_URL + `/login`, {});
+    //   console.log(res.data);
+    // } catch (err) {
+    //   console.error(err.response?.data || err.message);
+    // }
+    window.location.href = "http://127.0.0.1:3000/login";
   }
 
   return (
     <>
       <button onClick={getLogin}>log in to spotify</button>
+      <button onClick={getSpotifyTop}>get spotify</button>
       <Logo />
       <BoardingPass tracks={tracks} />
     </>
