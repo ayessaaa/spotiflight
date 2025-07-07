78d0daa881b53b3a37f76f23a61bacc71c34a927
Ayessa <ayessamaedeguzman78@gmail.com>
2025-07-07 14:00:44 +0800

    server get top songs

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import randomstring from "randomstring";
 import querystring from "querystring";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const client_id = process.env.SPOTIFY_CLIENT_ID;
@@ -10,6 +11,8 @@ const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 
 const app = express();
 const port = 3000;
+
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -36,7 +39,6 @@ app.get("/login", function (req, res) {
       })
   );
 });
-
 
 app.get("/callback", async (req, res) => {
   const code = req.query.code || null;
@@ -70,6 +72,12 @@ app.get("/callback", async (req, res) => {
     );
 
     const { access_token, refresh_token } = response.data;
+    res.cookie("spotify_token", access_token, {
+      httpOnly: false,
+      maxAge: 3600 * 1000,
+      secure: true,
+      sameSite: "Lax",
+    });
     res.send({
       access_token,
       refresh_token,
@@ -80,3 +88,21 @@ app.get("/callback", async (req, res) => {
   }
 });
 
+app.get("/top-songs", async (req, res) => {
+  try {
+    const token = req.cookies.spotify_token;
+    const response = await axios.get(
+      "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=5",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {},
+      }
+    );
+    res.json(response.data);
+  } catch (err) {
+    console.error("Spotify search failed:", err.message);
+    res.status(500).json({ error: "Spotify search error" });
+  }
+});
